fd56a1c2618ad0bc3aaf48802d63f45fc22bbbcf
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-11-08 10:26:43 -0800

    remove info

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 Credentials for Kaggle API
 """
 
-username = "mdominguez2010"
-key = "598764c7d15326e0fc47f7fb610fe86f"
+username = ""
+key = ""
 
 kaggle_json = {
-    "username":"mdominguez2010",
-    "key":"598764c7d15326e0fc47f7fb610fe86f"
+    "username":"",
+    "key":""
 }
